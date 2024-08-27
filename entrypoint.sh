18b58cc468e99f4cadfc36bb94e79e86b9d30426
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-08-27 14:53:03 -0400

    fix: support use of FLY_APP

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ GITHUB_REPOSITORY_NAME=${GITHUB_REPOSITORY#$GITHUB_REPOSITORY_OWNER/}
 EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
 
 # Default the Fly app name to pr-{number}-{repo_owner}-{repo_name}
-app="${INPUT_NAME:-pr-$PR_NUMBER-$GITHUB_REPOSITORY_OWNER-$GITHUB_REPOSITORY_NAME}"
+app="${INPUT_NAME:-${FLY_APP:-pr-$PR_NUMBER-$GITHUB_REPOSITORY_OWNER-$GITHUB_REPOSITORY_NAME}}"
 # Change underscores to hyphens.
 app="${app//_/-}"
 region="${INPUT_REGION:-${FLY_REGION:-iad}}"
